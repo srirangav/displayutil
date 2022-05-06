e37cadf4583807666000849734caacb3aacac209
Sriranga Veeraraghavan <srirangav@users.noreply.github.com>
2022-05-06 01:59:56 -0700

    add relevant references

--- displayutil_resolution.m
+++ displayutil_resolution.m
@@ -12,22 +12,23 @@
     Copyright (c) 2022 Sriranga R. Veeraraghavan <ranga@calalum.org>
 
     Permission is hereby granted, free of charge, to any person obtaining
-    a copy of this software and associated documentation files (the "Software"),
-    to deal in the Software without restriction, including without limitation
-    the rights to use, copy, modify, merge, publish, distribute, sublicense,
-    and/or sell copies of the Software, and to permit persons to whom the
-    Software is furnished to do so, subject to the following conditions:
+    a copy of this software and associated documentation files (the
+    "Software"), to deal in the Software without restriction, including
+    without limitation the rights to use, copy, modify, merge, publish,
+    distribute, sublicense, and/or sell copies of the Software, and to
+    permit persons to whom the Software is furnished to do so, subject to
+    the following conditions:
 
     The above copyright notice and this permission notice shall be included
     in all copies or substantial portions of the Software.
 
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-    OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-    DEALINGS IN THE SOFTWARE.
+    OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #import <stdio.h>
@@ -252,7 +253,18 @@ bool setResolutionForDisplay(unsigned long display,
         return ret;
     }
 
-    /* a matching resolution is available */
+    /*
+        A matching resolution is available; try to set the
+        display's resolution to the requested resolution
+
+        Based on:
+
+        https://gist.github.com/bellbind/27220148ea752335259dfce63f43bd5d#file-displaymode-c
+        https://developer.apple.com/documentation/coregraphics/1455235-cgbegindisplayconfiguration?language=objc
+        https://developer.apple.com/documentation/coregraphics/1454273-cgconfiguredisplaywithdisplaymod?language=objc
+        https://developer.apple.com/documentation/coregraphics/1454760-cgdisplaysetdisplaymode?language=objc
+        https://developer.apple.com/documentation/coregraphics/1454488-cgcompletedisplayconfiguration?language=objc
+    */
 
     if (CGBeginDisplayConfiguration(&config) != kCGErrorSuccess)
     {
